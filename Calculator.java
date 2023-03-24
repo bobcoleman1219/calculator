0504cd1f54963a1fee2809aa7378201363350a33
Bob Coleman <bob_coleman@sbcglobal.net>
2023-03-24 11:37:41 -0700

    Adding division feature

--- Calculator.java
+++ Calculator.java
@@ -17,5 +17,13 @@ public class Calculator {
 		return a * b;
 	}
 
+	public int divide (int a, int b) {
+		return a / b;
+	}
+
+	public double divide (double a, double b) {
+		return a / b;
+	}
+
 
 } 
